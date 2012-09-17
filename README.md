58b94a5f8ea37d2cf788b11e0d535bc4eb376f26
GeorgeStrakhov <george.strakhov@gmail.com>
2012-09-16 21:26:06 -0400

    still playing with content

--- README.md
+++ README.md
@@ -12,4 +12,55 @@ Clean & Simple
 
 Реализация
 ==========
-Bootstrap
+Bootstrap + BackBone?
+Concept: "telescopic" information. browsing within one page.
+Levels of information are represented by blocks on a page.
+
+Node Level 0 (topline) information is always available.
+
+every click on a link -> function (
+* detect the level from the which level the link is coming from
+* apply changes to 'this'
+* find & hide all this.
+)
+
+IF (you click on a link on Node level X)
+THEN (
+* current Node level X block changes color
+* current link that was clicked changes color
+* all the currently visible blocks for Nodes levels > X are destroyed
+* a new block for Node level X+1 is created and filled with content corresponding to that link
+)
+
+Source: XML(?) of JSON(?)
+
+e.g.
+    {
+        "title": "Мануфактура Социальных Коммуникаций",
+        "content": {
+            "type": "hypertext",
+            "content": [
+                {
+                    "content": "Привет!",
+                    "style": "h1",
+                    "children": [
+                        {
+                            "type": "hypertext",
+                            "content": [
+                                {
+                                    "content" : "А Вы кто?",
+                                    "style": "h1"
+                                }
+                            ]
+                        }
+                    ]
+                },
+                {
+                    "content": "Мы - "
+                },
+                {
+                   "content": "Мануфактура Социальных Коммуникаций."
+                }
+            ]
+        }
+    }
--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,59 @@
-$(document).ready( function() {
+$(function(){ //DOM is ready
+
+    /*load json file (string) with content & turn it into an object*/
+    $.ajax({
+        url:"content.js",
+        async:false,
+        dataType:"json",
+        success: function(response) {
+            var allContent = $.parseJSON(response);
     
-});
+            /*create backbone model for a single level*/
+            var SingleLevelModel = Backbone.Model.extend({
+                defaults: function() {
+                    return {
+                        type: "text",
+                        content: "warning: empty level",
+                        html_content: "<p>warning: empty level</p>",
+                    };
+                },
+                initialize: function() {
+                    console.log(allContent);
+                },
+            });
+            
+            /*recursively walk through the content object and instantiate backbone models for all levels of the object with all except the 0 level being "collapsed"*/
+            
+            /*add binding so that views are created and destroyed as models are "expanded" and "collapsed"*/
+            
+            /*create view for a single level and add listening to links (?)*/
+            
+            var SingleLevelView = Backbone.View.extend({
+                tagName: "div",
+                template: _.template($("#single_level_template").html()),
+                events: {},
+                initialize: function() {
+                    this.model.bind('change', this.render, this);
+                },
+                render: function() {
+                    this.$el.html(this.template(this.model.toJSON()));
+                    return this;
+                },
+            });
+            
+            /*instantiate level_0 view*/
+            
+            var level_0 = new SingleLevelModel({content: "0"});
+            
+            //console.log(level_0.get("type"));
+            
+            var view_0 = new SingleLevelView({model: level_0});
+            
+            $("#container").append(view_0.render().el);
+            
+            
+            level_0.set({"html_content":"<p>hellonewworld</p>"});
+        
+        } // end of response for ajax to get content.json
+    }); // end of ajax to get content.json
+}); //end of document.ready
